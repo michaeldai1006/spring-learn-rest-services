b0f7e9b91a9af8ad07f14503a07c9356c58799b2
michaeldai1006 <dtc74110@gmail.com>
2020-05-09 12:59:52 -0400

    Init

---
+++ src/main/java/com/example/tdlearnspringrestservices/TdlearnSpringRestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdlearnspringrestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlearnSpringRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdlearnSpringRestServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tdlearnspringrestservices/TdlearnSpringRestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdlearnspringrestservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdlearnSpringRestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
